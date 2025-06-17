f5f3a3223f31eab6adb681446c6fe7d373ac6bec
MatheuSoaresMoreira <matheus22smoreira@hotmail.com>
2025-06-17 14:06:40 -0300

    Teste2V1-src

--- src/main/java/com/room_reservations/RoomReservationsApplication.java
+++ src/main/java/com/room_reservations/RoomReservationsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RoomReservationsApplication {
+public class RoomReservationsApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(RoomReservationsApplication.class, args);
